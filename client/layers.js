5bad2926b03cdcbbb7ea81585f487562b62c939c
MarkPThomas <markums@gmail.com>
2014-02-17 14:53:44 -0800

    Rework to not use meteor

--- client/layers.js
+++ client/layers.js
@@ -1,13 +1,49 @@
-var prepareLayers = function () {
-  // prepare layers
+var unproject = function (coords) {
+  return map.unproject(coords, map.getMaxZoom());
 };
 
+_marker = function (coords, options) {
+  options = _.extend({icon: myIcon}, options);
+  return L.marker(unproject(coords), options);
+};
 
-Deps.autorun(function (c) {
-  if (Session.equals("map-ready", true)) {
-    c.stop();
+_circle = function (center, radius, options) {
+  options = _.extend({}, options);
+  return L.circle(unproject(center), radius, options);
+};
 
-    // Do all one-time stuff here
-    prepareLayers();
-  }
-});
+_polygon = function (coordsList, options) {
+  options = _.extend({}, options);
+  return L.polygon(_.map(coordsList, function (coords) {
+    return unproject(coords); 
+  }), options);
+};
+
+_polyline = function (coordsList, options) {
+  options = _.extend({}, options);
+  return L.polyline(_.map(coordsList, function (coords) {
+    return unproject(coords); 
+  }), options);
+};
+
+var prepareLayers = function () {
+  var marker = _marker([8000, 4000]).addTo(map);
+ 
+  var circle = _circle([4000, 1000], 200, {
+   color: 'red',
+   fillColor: '#f03',
+   fillOpacity: 0.5
+  }).addTo(map);
+
+  var polygon = _polygon([
+  [5001.509, 1000],
+  [5001.503, 2000],
+  [6000, 2500]
+  ]).addTo(map);
+
+  var polyline = _polyline([
+  [2000, 1000],
+  [2000, 2000],
+  [3000, 2500]
+  ]).addTo(map);
+};
--- client/map.js
+++ client/map.js
@@ -1,15 +1,18 @@
 // Guided by http://omarriott.com/aux/leaflet-js-non-geographical-imagery/
 // and using https://github.com/rktjmp/tileup
 
-Template.map.rendered = function () {
+var map, myIcon;
+
+var initialize = function () {
   // maximum maxZoom is 18 because it seems that L.CRS.Simple only
   // maps pixels to latLngs as far as that zoom level. minZoom should be
   // >= 0 for the same reason.
   // http://leafletjs.com/reference.html#tilelayer
-  var map = L.map('map', {
-    maxZoom: 18,
-    minZoom: 14,
-    crs: L.CRS.Simple
+  
+  map = L.map('map', {
+  maxZoom: 18,
+  minZoom: 14,
+  crs: L.CRS.Simple
   }).setView([0, 0], 14);
 
   var southWest = map.unproject([0, 8406], map.getMaxZoom());
@@ -18,9 +21,23 @@ Template.map.rendered = function () {
 
   // map_tiles directory not included with repo because map tiles
   // take up ~ 200MB.
-  L.tileLayer('map_tiles/{z}/map_tile_{x}_{y}.jpg', {
-    attribution: 'Map data &copy; Mark Porter Thomas'
+  L.tileLayer('../public/map_tiles/{z}/map_tile_{x}_{y}.jpg', {
+  attribution: 'Map data &copy; Mark Porter Thomas'
   }).addTo(map);
 
-  Session.set("map-ready", true);
+  myIcon = L.icon({
+    iconUrl: '../public/images/marker-icon.png',
+    iconRetinaUrl: '../public/images/marker-icon@2x.png',
+    //iconSize: [38, 95],
+    //iconAnchor: [22, 94],
+    //popupAnchor: [-3, -76],
+    shadowUrl: '../public/images/marker-shadow.png',
+    //shadowSize: [68, 95],
+    //shadowAnchor: [22, 94]
+  });
 };
+
+window.onload = function () {
+  initialize();
+  prepareLayers();
+}
